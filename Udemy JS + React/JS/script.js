3ae3d3559e0d8bc2993a802359eb06edd3761c91
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-02-02 16:51:18 +0200

    Communication with the user

---
+++ Udemy JS + React/JS/script.js
@@ -0,0 +1,17 @@
+"use sctrict";
+
+//alert('hello');
+
+//const res = confirm("Are you here?");
+//console.log(res);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+
+const answer = [];
+
+answer[0] = prompt('Как ваше имя?');
+answer[1] = prompt('Какая ваша фамилия?');
+answer[2] = prompt('Сколько вам лет?');
+
+console.log(answer);
