c75d2b20435b2eb18a7c9174655ce6f5d85d9e9e
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-02-21 15:29:08 +0200

    Array

---
+++ Udemy JS + React/JS/Array/Array.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const arr = [2, 13, 26, 8, 10];
+
+arr.sort(compareNum);
+arr.pop();
+arr.push(2);
+
+function compareNum(a,b){
+    return a - b;
+}
+console.log(arr);
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+for(let value of arr){
+    console.log(value);
+}
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
