5863884371e8b1d3bd873120e605a9622ee88261
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-02-23 17:31:40 +0200

    OOP

---
+++ Udemy JS + React/JS/OOP/OOP.js
@@ -0,0 +1,23 @@
+'use sctrict';
+
+let str = "some";
+let strObj = new String(str);
+
+//console.log(typeof(str));
+//console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+const soldier = {
+    health: 400,
+    armor: 100
+};
+
+const jonh = Object.create(soldier);
+
+
+jonh.__proto__ = soldier;
+
+Object.setPrototypeOf(jonh, soldier);
+
+console.log(jonh.armor);
