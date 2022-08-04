50e788769dc5e521914e05946faffab3b0c31258
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-08-04 14:26:12 +0300

    Added Interview tasks

---
+++ Udemy JS + React/JS/Interview tasks for understanding the basics/InterviewTask.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//let x =  5; alert( x++ );
+// [ ] + false - null + true
+//console.log([ ] + false - null + true); // NaN
+
+//let y = 1;
+//let x = y = 2;
+//alert(x);
+
+//console.log([] + 1 +2); //12
+
+//alert("1"[0]); //1
+
+//console.log(2 && 1 && null && 0 && undefined); //null
+
+//console.log(!!(1 && 2) ===(2 && 1));
+
+// alert(null || 2 && 3 || 4); //3
+
+// const a = [1, 2, 3]; 
+// const b = [1, 2, 3];
+
+// console.log(a == b);
+
+// alert(+"infinity");
+
+// console.log("Ёжик" > "яблоко");//false
+
+//console.log(0 || "" || 2 || undefined || true || false);
